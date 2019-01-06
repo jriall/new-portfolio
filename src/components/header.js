6f9373b173e2f296a43f7327c74f4b85d30fd95c
James Riall <jamesrariall@gmail.com>
2019-01-06 22:50:11 +0000

    Convert styling to use styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,36 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div`
+  background-color: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const HeaderContent = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+const HeaderTitle = styled.h1`
+  margin: 0;
+`;
+
+const HeaderLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderContainer>
+    <HeaderContent>
+      <HeaderTitle>
+        <HeaderLink to="/">{siteTitle}</HeaderLink>
+      </HeaderTitle>
+    </HeaderContent>
+  </HeaderContainer>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
 import './layout.css'
+
+const PageContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,21 +26,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <PageContainer>
           {children}
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
-        </div>
+        </PageContainer>
       </>
     )}
   />
